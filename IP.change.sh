331ef61ee4d0cfb11ea109d10be54ac39231ba5b
TuDouYiHao <49424600+TuDouYiHao@users.noreply.github.com>
2019-06-13 10:19:35 +0800

    Add files via upload
    更改为静态IP

---
+++ IP.change.sh
@@ -0,0 +1,32 @@
+﻿#!/usr/bin/env bash
+#
+#Author:BlackXYQ
+#Date:2019/4/2
+# Use: change network
+
+
+read -p "Pleace inpute your ip : "  ip
+
+
+echo "
+TYPE=Ethernet
+PROXY_METHOD=none
+BROWSER_ONLY=no
+BOOTPROTO=none			
+DEFROUTE=yes
+IPV4_FAILURE_FATAL=no
+NAME=ens33
+DEVICE=ens33
+ONBOOT=yes
+IPADDR=$ip
+PREFIX=24
+GATEWAY=10.3.145.1
+DNS1=114.114.114.114
+    " > /etc/sysconfig/network-scripts/ifcfg-ens33
+
+if [ $? -eq 0 ];then
+
+systemctl restart network
+else
+printf "\e[1;33m你的ip地址有问题！！！\e[0m"
+fi
